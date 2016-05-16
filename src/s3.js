ff149b9e6802b74497ade058e4d964ff9ee0aee2
windsource <windsource@gmx.de>
2016-05-16 14:50:40 +0200

    Added size calculation of objects in one bucket.

--- src/s3.js
+++ src/s3.js
@@ -1,14 +1,28 @@
 'use strict';
 
-
 const AWS = require('aws-sdk');
 
 class S3 {
+  /**
+   * Create a new object to work with AWS S3.
+   * @param {string} region AWS region like 'eu-west-1', 'eu-central-1', etc.
+   */
   constructor(region) {
     this.region = region;
-    this.s3 = new AWS.S3();
+    if (this.region === 'eu-central-1') {
+      AWS.config.update({ region: this.region, signatureVersion: 'v4' });
+      const ep = new AWS.Endpoint('s3.eu-central-1.amazonaws.com');
+      this.s3 = new AWS.S3({ endpoint: ep });
+    } else {
+      AWS.config.update({ region: this.region });
+      this.s3 = new AWS.S3();
+    }
   }
 
+  /**
+   * List all the buckets
+   * @returns {Promise} that returns the AWS output.
+   */
   listBuckets() {
     return new Promise((fulfill, reject) => {
       this.s3.listBuckets((err, data) => {
@@ -18,6 +32,11 @@ class S3 {
     });
   }
 
+  /**
+   * Create a new bucket
+   * @param {string} name of the bucket
+   * @returns {Promise} that returns the AWS output.
+   */
   createBucket(name) {
     return new Promise((fulfill, reject) => {
       const params = {
@@ -33,6 +52,11 @@ class S3 {
     });
   }
 
+  /**
+   * Delete an existing bucket
+   * @param {string} name of the bucket
+   * @returns {Promise} that returns the AWS output.
+   */
   deleteBucket(name) {
     return new Promise((fulfill, reject) => {
       const params = {
@@ -41,6 +65,59 @@ class S3 {
       this.s3.deleteBucket(params, (err, data) => {
         if (err) reject(err);
         else fulfill(data);
+      });
+    });
+  }
+
+  /**
+   * List the objects in a bucket
+   * @param {string} bucketName name of the bucket
+   * @param {string} continuationToken used from previous request to get the remaining objects
+   * @returns {Promise} that returns the AWS output.
+   */
+  listObjects(bucketName, continuationToken) {
+    return new Promise((fulfill, reject) => {
+      const params = {
+        Bucket: bucketName,
+        ContinuationToken: continuationToken,
+        MaxKeys: 1000,
+      };
+      this.s3.listObjectsV2(params, (err, data) => {
+        if (err) reject(err);
+        else fulfill(data);
+      });
+    });
+  }
+
+  /**
+   * Get the size of a bucket (i.e. the sum of all objects' size in that bucket)
+   * @param {string} bucketName name of the bucket
+   * @returns {Promise} that returns the size.
+   */
+  getBucketSize(bucketName) {
+    return this._calcBucketSize(bucketName);
+  }
+
+  _calcBucketSize(bucketName, prevSize, continuationToken) {
+    console.log('eins');
+    return new Promise((fulfill, reject) => {
+      let size = prevSize || 0;
+      this.listObjects(bucketName, continuationToken).then((listRes) => {
+        for (const o of listRes.Contents) {
+          size += o.Size;
+        }
+        if (listRes.hasOwnProperty('NextContinuationToken')) {
+          this._calcBucketSize(bucketName, size, listRes.NextContinuationToken).then((calcRes) => {
+            fulfill(calcRes);
+          }, (err) => {
+            reject(err);
+          });
+        } else {
+          // size calculation is finished
+          fulfill(size);
+        }
+      }, (err) => {
+        reject(err);
       });
     });
   }
--- test/s3_test.js
+++ test/s3_test.js
@@ -4,29 +4,43 @@ const S3 = require('../src/s3.js');
 
 describe('Testing S3', () => {
   let s3;
-  const newBucketName = 'dummybxucketbghbjgrjgajrghjarsfh';
+  const newBucketName = 'dum33mybxucketbgh555bjggajrghjarsfh';
 
   before(() => {
-    s3 = new S3('eu-west-1');
+    s3 = new S3('eu-central-1');
   });
 
   it('should create a new bucket', (done) => {
     s3.createBucket(newBucketName).then(() => {
       done();
-    });
+    }, done);
   });
 
   it('should list my buckets', (done) => {
     s3.listBuckets().then((res) => {
       console.log(res);
       done();
-    });
+    }, done);
+  });
+
+  it('should list objects in bucket', (done) => {
+    s3.listObjects(newBucketName).then((res) => {
+      console.log(res);
+      done();
+    }, done);
+  });
+
+  it('should get size of bucket', (done) => {
+    s3.getBucketSize(newBucketName).then((res) => {
+      console.log(res);
+      done();
+    }, done);
   });
 
   it('should delete the new bucket', (done) => {
     s3.deleteBucket(newBucketName).then(() => {
       done();
-    });
+    }, done);
   });
 
   after(() => {
